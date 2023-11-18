0fe70049e055b7c46127e1e56ecce7b7af3cb232
Sasidharan G S <SasidharanGS.701@gmail.com>
2023-11-18 17:19:21 +0530

    IST timezone

--- modded_influx/serialToCsv.py
+++ modded_influx/serialToCsv.py
@@ -2,6 +2,8 @@ import serial
 import csv
 import re
 import time
+import pytz
+from datetime import datetime  # Import datetime module
 
 serial_port = '/dev/ttyUSB0'
 
@@ -9,20 +11,24 @@ ser = serial.Serial(serial_port, 115200)
 
 column_names = ['Timestamp', 'Voltage (V)', 'Current (A)', 'Power (W)', 'Energy (Wh)', 'Frequency (Hz)', 'People Inside']
 
+ist = pytz.timezone('Asia/Kolkata')  # Set Indian Standard Time
+
 with open('data.csv', 'a', newline='') as csv_file:
-    csv_writer = csv.writer(csv_file)
+    csv_writer = csv.writer(csv_file, lineterminator='\n')
 
     if csv_file.tell() == 0:
         csv_writer.writerow(column_names)
-    
-    timestamp = time.strftime('%d-%m-%Y -> %H:%M:%S', time.gmtime())
 
     try:
         while True:
+            utc_now = datetime.utcnow().replace(tzinfo=pytz.utc)  # Get current UTC time
+            ist_now = utc_now.astimezone(ist)  # Convert to IST
+            timestamp = ist_now.strftime('%d-%m-%Y -> %H:%M:%S')
+
             line = ser.readline().decode('utf-8')
-            print(timestamp+"\n"+line)
+            print(timestamp + "\n" + line)
             numeric_values = re.findall(r"[-+]?\d*\.\d+|\d+", line)
-            
+
             if len(numeric_values) == len(column_names) - 1:
                 csv_writer.writerow([timestamp] + numeric_values)
 
